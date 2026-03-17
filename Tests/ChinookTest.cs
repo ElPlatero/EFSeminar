9f2767c7fb0ecb91daf3b4b785d6cdeca5ed2d49
Tino Schüttpelz <tino.schuettpelz@icomps.de>
2026-03-17 15:18:11 +0100

    Übung 2

--- Tests/ChinookTest.cs
+++ Tests/ChinookTest.cs
@@ -21,6 +21,38 @@ public partial class ChinookTest(ITestOutputHelper helper)
 
     }
 
+    [Theory]
+    [InlineData("Aerosmith")]
+    public async Task TestGetAlbums(string artistName)
+    {
+        await using var context = getContext();
 
-    private static partial ChinookContext getContext();
+        var result = await context.Albums.Where(p => p.Artist.Name == artistName).Select(p => p.Title).ToListAsync();
+        result.ForEach(p => helper.WriteLine($"Album: {p}"));
+        
+    }
+
+    [Theory]
+    [InlineData("Eric Clapton")]
+    public async Task TestGetAlbumsWithTracks(string artistName)
+    {
+        await using var context = getContext();
+
+        var result = await context.Albums.Where(p => p.Artist.Name == artistName).Select(p => new { p.Title, p.Tracks }).ToListAsync();
+        result.ForEach(p => helper.WriteLine($"{p.Title} hat die Tracks {string.Join(", ", p.Tracks.Select(q => q.Name))}."));
+    }
+
+    [Theory]
+    [InlineData("Blues")]
+    public async Task GetArtistsOfGenre(string genreName)
+    {
+        await using var context = getContext();
+        
+        var artists = await context.Genres.Where(p => p.Name == genreName).SelectMany(p => p.Tracks.Where(q => q.Album != null).Select(q => q.Album!.Artist )).Select(p => p.Name).ToHashSetAsync();
+        helper.WriteLine($"Künstler: {string.Join(", ", artists)}");
+    }
+
+    
+
+    private partial ChinookContext getContext();
 }
