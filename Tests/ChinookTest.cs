8f1455769d483cf76135cd83b8674780a784de6a
Tino Schüttpelz <tino.schuettpelz@icomps.de>
2026-03-17 14:00:11 +0100

    Unit Test

--- Tests/ChinookTest.cs
+++ Tests/ChinookTest.cs
@@ -1,9 +1,26 @@
-﻿namespace Tests;
+﻿using EntityFrameworkCoreSeminar.Database;
+using EntityFrameworkCoreSeminar.Database.Models.Chinook;
+using Microsoft.EntityFrameworkCore;
+using Xunit.Abstractions;
 
-public class ChinookTest
+namespace Tests;
+
+public partial class ChinookTest(ITestOutputHelper helper)
 {
     [Fact]
-    public void Test1()
+    public async Task TestGetArtists()
     {
+        await using var context = getContext();
+        var result = await context.Artists.Select(p => new { p.Name, p.Albums }).OrderBy(p => p.Name).Skip(2).Take(10).ToListAsync();
+
+        Assert.NotNull(result);
+        foreach (var artist in result)
+        {
+            helper.WriteLine($"{artist.Name} hat {artist.Albums.Count} Alben.");
+        }
+
     }
+
+
+    private static partial ChinookContext getContext();
 }
